ee1316d1803950f3d76026cf7f460a49e1c91bd5
developernagor <developernagor@gmail.com>
2025-01-16 14:07:43 +0600

    Get property data from db

--- src/pages/Dashboard/AgentDashboard/MyAddedProperties.jsx
+++ src/pages/Dashboard/AgentDashboard/MyAddedProperties.jsx
@@ -1,6 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 function MyAddedProperties() {
+    const {isPending, data:properties} = useQuery({
+        queryKey: ['properties'],
+        queryFn: async()=> {
+            const res = await fetch('http://localhost:5000/properties');
+            return res.json();
+        }
+    });
+    
+
+    if(isPending){
+        return 'loading......'
+    }
+
+    console.log(properties)
+
+
     return (
         <div>
             My Added Properties
